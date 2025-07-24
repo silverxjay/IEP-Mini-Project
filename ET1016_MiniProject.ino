c7b4c19da50556799a8862529cb2a12e0f3d7065
silverxjay <jaymak232@yahoo.com>
2025-07-24 15:42:25 +0800

    Add files via upload

---
+++ ET1016_MiniProject.ino
@@ -0,0 +1,15 @@
+// Title
+// Mak Si Jie, Harold Mah
+// 2516477, 2517100
+// DCEP/FT/1A/09
+// Description
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
